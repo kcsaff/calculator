84ec6321911d8fc2129eb0bab67d561950a64169
Kevin Saff <kevin.saff@gmail.com>
2015-06-21 20:21:01 -0700

    Simplify

--- calculator.py
+++ calculator.py
@@ -53,29 +53,16 @@ class Calculator(object):
         self.max_precount = max(O.precount for O in operators)
         self.tokenize = tokenize
 
-    def calculate(self, expression):
-        tokens = self.tokenize(expression)
-        return self._eval(tokens)[0]
-
     def __call__(self, expression):
         return self.calculate(expression)
 
-    def _interpret(self, token):
-        for interpreter in self.interpreters:
-            try:
-                value = interpreter(token)
-            except Exception as err:
-                pass
-            else:
-                return value
-        else:
-            raise ValueError('Could not interpret %r' % token)
-
-    def _eval(self, tokens, stop=(lambda X: not X), precedence=0, valuecount=1):
+    def calculate(self, tokens, stop='', precedence=0):
+        if not hasattr(tokens, 'get_token'):
+            tokens = self.tokenize(tokens)
         values = list()
-        while True:
+        while len(values) <= 1:
             token = tokens.get_token()
-            if stop(token):
+            if token == stop:
                 break
             for argcount in range(len(values), -1, -1):
                 if (token, argcount) in self.operators:
@@ -93,13 +80,24 @@ class Calculator(object):
                 break
             else:
                 args = values[len(values)-operator.precount:]
-                values = values[:len(values)-operator.precount] + [operator.calculate(self._eval, tokens, token, stop, *args)]
+                values = values[:len(values)-operator.precount] + [operator.process(self.calculate, tokens, token, stop, *args)]
 
         tokens.push_token(token)
-        if len(values) == valuecount:
-            return values
+        if len(values) == 1:
+            return values[0]
         else:
             raise ValueError('No operator takes {} {} values: {}'.format(token, len(values), values))
+
+    def _interpret(self, token):
+        for interpreter in self.interpreters:
+            try:
+                value = interpreter(token)
+            except Exception as err:
+                pass
+            else:
+                return value
+        else:
+            raise ValueError('Could not interpret %r' % token)
 
 
 def _apply_or_mul(left, right):
@@ -117,61 +115,54 @@ _default_interpreters = (
 class Operator(object):
     ADJACENT = object()
 
-    def __init__(self, *parts):
-        if isinstance(parts[0], str):
-            self.trump, self.precount, self.token = float("inf"), 0, parts[0]
-            self.parts = parts[1:]
+    def __init__(self, *precedences):
+        if isinstance(precedences[0], str):
+            self.trump, self.precount, self.token = float("inf"), 0, precedences[0]
+            self.precedences = precedences[1:-1]
         else:
-            self.trump, self.precount, self.token = parts[:3]
-            self.parts = parts[3:]
-        self.action = (lambda X: X)
+            self.trump, self.token = precedences[:2]
+            self.precount = 1
+            self.precedences = precedences[2:-1]
+        self.action = precedences[-1] or (lambda X: X)
 
-    def __call__(self, action):
-        self.action = action
-        return self
-
-    def calculate(self, evaluate, tokens, token, stop, *args):
+    def process(self, evaluate, tokens, token, stop, *args):
         if self.token is None:
             tokens.push_token(token)
         args = list(args)
-        for i in range(0, len(self.parts), 2):
-            argcount = self.parts[i]
-            precedence = self.parts[i+1] if len(self.parts) > i+1 else None
-            if precedence is None: #Postfix
-                pass
-            elif isinstance(precedence, str): #group
-                args.extend(evaluate(tokens, (lambda X: X == precedence), 0, argcount))
+        for precedence in self.precedences:
+            if isinstance(precedence, str): #group
+                args.append(evaluate(tokens, precedence, 0))
                 end_token = tokens.get_token()
                 if end_token != precedence:
                     raise ValueError('Mismatched group: %s...%s != %s' % (self.token, end_token, precedence))
             else:
-                args.extend(evaluate(tokens, stop, precedence, argcount))
+                args.append(evaluate(tokens, stop, precedence))
         return self.action(*args)
 
 
 _default_operators = [
-    Operator(510, 1, '=', 1, 500)(lambda L,R: L.assign(R)),
-    Operator(1090, 1, '==', 1, 1100)(operator.eq),
-    Operator(1090, 1, '<=', 1, 1100)(operator.le),
-    Operator(1090, 1, '>=', 1, 1100)(operator.ge),
-    Operator(1090, 1, '!=', 1, 1100)(operator.ne),
-    Operator(1090, 1, '<', 1, 1100)(operator.lt),
-    Operator(1090, 1, '>', 1, 1100)(operator.gt),
-    Operator(2090, 1, '+', 1, 2100)(operator.add),
-    Operator(2090, 1, '-', 1, 2100)(operator.sub),
-    Operator(2190, 1, '*', 1, 2200)(operator.mul),
-    Operator(2190, 1, '/', 1, 2200)(operator.truediv),
-    Operator(2310, 1, '^', 1, 2300)(operator.pow),
-    Operator(2390, 1, None, 1, 2400)(_apply_or_mul),
+    Operator(510, '=', 500, lambda L,R: L.assign(R)),
+    Operator(1090, '==', 1100, operator.eq),
+    Operator(1090, '<=', 1100, operator.le),
+    Operator(1090, '>=', 1100, operator.ge),
+    Operator(1090, '!=', 1100, operator.ne),
+    Operator(1090, '<', 1100, operator.lt),
+    Operator(1090, '>', 1100, operator.gt),
+    Operator(2090, '+', 2100, operator.add),
+    Operator(2090, '-', 2100, operator.sub),
+    Operator(2190, '*', 2200, operator.mul),
+    Operator(2190, '/', 2200, operator.truediv),
+    Operator(2310, '^', 2300, operator.pow),
+    Operator(2390, None, 2400, _apply_or_mul),
     # Call-like
-    Operator(3000, 1, '[', 1, ']')(operator.getitem),
+    Operator(3000, '[', ']', operator.getitem),
     # Postfix
-    Operator(2295, 1, '%')(lambda x: x * 0.01),
+    Operator(2295, '%', lambda x: x * 0.01),
     # Prefix
-    Operator('+', 1, 2305)(operator.pos),
-    Operator('-', 1, 2305)(operator.neg),
+    Operator('+', 2305, operator.pos),
+    Operator('-', 2305, operator.neg),
     # Grouping
-    Operator('(', 1, ')'),
+    Operator('(', ')', None),
 ]
 
 
